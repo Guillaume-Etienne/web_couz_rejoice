b149e2bc1179a4df00ba02e05a8af1be78b77b00
Guillaume <gsetienne@free.fr>
2025-06-11 20:33:19 +0200

    MAJ EN et correction bug JS OK

--- js/rejoice.js
+++ js/rejoice.js
@@ -1,3 +1,7 @@
+// Toggle language   / Toglle des boutons pour les iframes des dons et contactez-nous
+
+
+// ---------------------- Toggle language
 document.querySelectorAll('.language-option').forEach(option => {
         option.addEventListener('click', function(e) {
             e.preventDefault();
@@ -9,3 +13,23 @@ document.querySelectorAll('.language-option').forEach(option => {
             }
         });
     });
+
+
+// -------------------------------------  Toggle des boutons pour les iframes des dons et contactez-nous
+// Récupérez tous les boutons et le texte supplémentaire
+const toggleButtons = document.querySelectorAll('.toggle-button');
+const additionalTexts = document.querySelectorAll('.additional-text');
+
+// Ajoutez un gestionnaire d'événements de clic à chaque bouton
+toggleButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Affichez ou masquez le texte supplémentaire en fonction de son état actuel
+        if (additionalTexts[index].style.display === 'none' || additionalTexts[index].style.display === '') {
+            additionalTexts[index].style.display = 'block';
+            button.textContent = ' < ';
+        } else {
+            additionalTexts[index].style.display = 'none';
+            button.textContent = ' > ';
+        }
+    });
+});
